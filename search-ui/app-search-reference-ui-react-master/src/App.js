51abe5f7fd8cfc860fa2969a6bdc24aec2d547be
hys89 <10128883+hys89@users.noreply.github.com>
2025-01-12 15:20:47 +0800

    Update App.js

--- search-ui/app-search-reference-ui-react-master/src/App.js
+++ search-ui/app-search-reference-ui-react-master/src/App.js
@@ -17,7 +17,7 @@ import { Layout } from "@elastic/react-search-ui-views";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
 
 const connector = new ElasticsearchAPIConnector({
-  host: "http://localhost:9200",
+  host: "http://ec2-47-129-205-236.ap-southeast-1.compute.amazonaws.com:9200",
   index: "cv-transcriptions"
 });
 
